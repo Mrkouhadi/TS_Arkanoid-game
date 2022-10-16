77bc65df92513b2bfa8a7f2b64614faadfd39754
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-10-16 22:59:38 +0800

    i built the paddle class

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -1,3 +1,54 @@
 import { Vector } from "../types";
 
-export class Paddle{}
+export class Paddle{
+    private paddleImage:HTMLImageElement = new Image();
+    private movetoLeft:boolean;
+    private movetoRight:boolean;
+
+    constructor(
+                private speed:number, private paddleWidth:number,
+                private paddleHeight:number, private position:Vector,
+                private image:string
+                ){
+            this.paddleImage.src = image;
+            this.movetoLeft = false;
+            this.movetoRight = false;
+            // eventlistners
+            document.addEventListener('keydown', this.keydownHandler)
+            document.addEventListener('keyup', this.keyupHandler)
+        }
+
+    // getters
+    get width():number{
+        return this.paddleWidth;
+    }
+    get height():number{
+        return this.paddleHeight;
+    }
+    get pos():Vector{
+        return this.position;
+    }
+    get img():HTMLImageElement{
+        return this.paddleImage;
+    }
+    get isMovingLeft():boolean{
+        return this.movetoLeft
+    }
+    get isMovingRight():boolean{
+        return this.movetoRight
+    }
+
+    // methods
+    movePadlle():void{
+        if(this.movetoLeft) this.pos.x -= this.speed;
+        if(this.movetoRight) this.pos.x += this.speed;
+    }
+    keyupHandler=(e:KeyboardEvent):void=>{ // using arrow function so that we don't go through binding and that shit
+        if(e.code === "ArrowLeft" || e.key === "ArrowLeft") this.movetoLeft = false;
+        if(e.code === "ArrowRight" || e.key === "ArrowRight") this.movetoRight = false;
+    }
+    keydownHandler=(e:KeyboardEvent):void=>{ // using arrow function so that we don't go through binding and that shit
+        if(e.code === "ArrowLeft" || e.key === "ArrowLeft") this.movetoLeft = true;
+        if(e.code === "ArrowRight" || e.key === "ArrowRight") this.movetoRight = true;
+    }
+}
