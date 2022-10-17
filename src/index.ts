17129fbabe79ed34854a2407beadab8eceebb6e9
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-10-17 14:10:58 +0800

    implemented gameover functions

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,22 @@ import { PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_SPEED, PADDLE_STARTX } from "./setu
 import { BALL_SIZE, BALL_STARTY, BALL_SPEED, BALL_STARTX } from "./setup";
 // import helpers
 import { createBricks } from "./helpers";
+import { Collision } from "./collision";
 
-let gameover = false;
+let gameOver = false;
+
 let score = 0;
 
 const setGameOver=(view: CanvasView):void=>{
     view.drawInfo("The Game is Over !!!");
-    gameover = false;
+    gameOver = false;
 }
 const setGameWin=(view: CanvasView):void=>{
     view.drawInfo("The Game Won !!!");
-    gameover = false;
+    gameOver = false;
 }
 
-const gameLoop=(view: CanvasView, bricks:Brick[],paddle:Paddle,ball:Ball)=>{ //  , ball:Ball    
+const gameLoop=(view: CanvasView, bricks:Brick[],paddle:Paddle,ball:Ball, collsion:Collision)=>{ //  , ball:Ball    
     view.clearCanvas();
     view.drawBricks(bricks);
     view.drawSprite(paddle);
@@ -36,8 +38,23 @@ const gameLoop=(view: CanvasView, bricks:Brick[],paddle:Paddle,ball:Ball)=>{ // 
             paddle.movePadlle()
         }
 
+    collsion.checkBallCollision(ball, paddle,view)
+    const collidingBrick = collsion.isCollidingBricks(ball, bricks);
+
+    if (collidingBrick) {
+      score += 1;
+      view.drawScore(score);
+    }
+  
+    // Game Over when ball leaves playField
+    if (ball.pos.y > view.canvas.height) gameOver = true;
+    // If game won, set gameOver and display win
+    if (bricks.length === 0) return setGameWin(view);
+    // Return if gameover and don't run the requestAnimationFrame
+    if (gameOver) return setGameOver(view);
+
     requestAnimationFrame(()=>{
-        gameLoop(view, bricks,paddle,ball)
+        gameLoop(view, bricks,paddle,ball,collsion)
     });
 }
 
@@ -46,13 +63,16 @@ const startGame=(view: CanvasView)=>{
     score = 0;
     view.drawInfo("")
     view.drawScore(0);
+
+    // create colision instance
+    const collsion = new Collision()
     //  create bricks
     const bricks = createBricks()
     // create the Ball
     const ball = new Ball(BALL_SIZE, {x:BALL_STARTX, y: BALL_STARTY}, BALL_SPEED, BALL_IMAGE )
     // create the Paddle 
     const paddle = new Paddle( PADDLE_SPEED, PADDLE_WIDTH,PADDLE_HEIGHT,{x: PADDLE_STARTX, y:view.canvas.height - PADDLE_HEIGHT },PADDLE_IMAGE)
-    gameLoop(view, bricks,paddle, ball)
+    gameLoop(view, bricks,paddle, ball, collsion)
 }
 // generate a canvas view
 const view = new CanvasView('playField');
